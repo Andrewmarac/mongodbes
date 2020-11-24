eb3a2a607870dbb845909d1f61de1196e46db7d5
Andrewmarac <marasigan.joshua.andrew@iisgalvanimi.edu.it>
2020-11-24 08:11:10 +0000

    + bt

--- ESEMPIO_MONGO/mflix-visualizer/src/app/app.component.ts
+++ ESEMPIO_MONGO/mflix-visualizer/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
  selector: 'app-root',
  templateUrl: './app.component.html',
@@ -10,13 +11,18 @@ export class AppComponent {
  title = 'mflix-visualizer';
  results : Object[];
  obs : Observable<object>;
- constructor(private http : HttpClient){}
+ constructor(private http : HttpClient, private sanitizer: DomSanitizer){}
  load10Movies()
  {
-    this.obs = this.http.get("https://3000-e1838874-5d49-4c8c-b1df-3ca2e124eccf.ws-eu01.gitpod.io/movie");
+    this.obs = this.http.get("https://3000-f441531b-1e97-43b0-851d-5453e3955f89.ws-eu01.gitpod.io/movie");
     this.obs.subscribe(this.getData);
  }
     getData = (data) => {
     this.results = data;
  }
+ photoURL(urltoSanitize) {
+    console.log(urltoSanitize);
+    return this.sanitizer.bypassSecurityTrustUrl(urltoSanitize);
+}
+
 }
